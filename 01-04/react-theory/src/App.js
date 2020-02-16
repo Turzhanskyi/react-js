843cbbd6f2897ad7dafd515fed6c7cbcd76221ce
Turzhanskyi <turzhansky81@gmail.com>
2020-02-16 22:16:11 +0200

    6. Как работает JSX

--- 01-04/react-theory/src/App.js
+++ 01-04/react-theory/src/App.js
@@ -7,6 +7,18 @@ function App() {
       <h1>Hello World!!!</h1>
     </div>
   );
+
+  // return React.createElement(
+  //   'div',
+  //   {
+  //     className: 'App'
+  //   },
+  //   React.createElement(
+  //     'h1',
+  //     null,
+  //     'Hello world!'
+  //   )
+  // )
 }
 
 export default App;
