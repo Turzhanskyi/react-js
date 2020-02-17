5efd9fdf6f9c5d77b176add8a129f6ec21bb1b56
Turzhanskyi <turzhansky81@gmail.com>
2020-02-17 06:38:41 +0200

    8. Inline-стили

--- 01-04/react-theory/src/App.js
+++ 01-04/react-theory/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const divStyle = {
+    textAlign: 'center'
+  };
+
   return (
-      <div>
-        <div className="App">
-          <h1>Hello World!!!</h1>
-        </div>
-        <p>Hello</p>
+      <div style={divStyle}>
+        <h1 style={ {color: 'blue', fontSize: '20px'} }>Hello World!!!</h1>
       </div>
   );
 }
