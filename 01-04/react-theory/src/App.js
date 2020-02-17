4b42a0b3f630bda1ceb889401caaff76c6a13f6d
Turzhanskyi <turzhansky81@gmail.com>
2020-02-17 10:06:35 +0200

    13. Создание State

--- 01-04/react-theory/src/App.js
+++ 01-04/react-theory/src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Car from "./Car/Car";
 
-function App() {
-  const divStyle = {
-    textAlign: 'center'
+class App extends Component {
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010}
+    ],
+    pageTitle: 'React components'
   };
 
-  return (
-      <div style={divStyle}>
-        <h1>Hello World!!!</h1>
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    };
 
-        <Car name={'Ford'} year={2018}>
-          <p style={ {color: 'blue'} }>COLOR</p>
-        </Car>
-        <Car name="Audi" year="2016">
-          <p style={ {color: 'red'} }>COLOR</p>
-        </Car>
-        <Car name={'Mazda'} year={2010}/>
-      </div>
-  );
+    const cars = this.state.cars;
+
+    return (
+        <div style={divStyle}>
+          <h1>{this.state.pageTitle}</h1>
+
+          <Car name={cars[0].name} year={cars[0].year}/>
+          <Car name={cars[1].name} year={cars[1].year}/>
+          <Car name={cars[2].name} year={cars[2].year}/>
+        </div>
+    );
+  }
 }
 
 export default App;
